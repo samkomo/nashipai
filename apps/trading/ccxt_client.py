ea188adccb1be93360048aceac89c4f6e69a4af1
samkomo <karumba66@gmail.com>
2023-09-14 10:57:35 +1000

    Added a variable to set supported exchanges

--- apps/trading/ccxt_client.py
+++ apps/trading/ccxt_client.py
@@ -29,14 +29,17 @@ class TradingModule:
     - subaccount (str): The subaccount to use, if any. Defaults to None.
     """
     def setup_exchanges(self, subaccount=None, sandbox_mode=True):
-        for exchange_id in self.exchanges.keys():
+        # Fetch the list of supported exchanges from the environment variable and split it
+        supported_exchanges = os.environ.get('SUPPORTED_EXCHANGES', '').split(',')
+
+        for exchange_id in supported_exchanges:
             # Fetch API key and secret for the given exchange_id from environment variables
-            api_key = os.environ.get(f'{exchange_id}_API_KEY')
-            api_secret = os.environ.get(f'{exchange_id}_API_SECRET')
+            api_key = os.environ.get(f'{exchange_id.upper()}_API_KEY')
+            api_secret = os.environ.get(f'{exchange_id.upper()}_API_SECRET')
             
             # If there's a subaccount, try to fetch its specific API key and secret
-            subaccount_api_key = os.environ.get(f'{exchange_id}_{subaccount}_API_KEY', api_key)
-            subaccount_api_secret = os.environ.get(f'{exchange_id}_{subaccount}_API_SECRET', api_secret)
+            subaccount_api_key = os.environ.get(f'{exchange_id.upper()}_{subaccount}_API_KEY', api_key)
+            subaccount_api_secret = os.environ.get(f'{exchange_id.upper()}_{subaccount}_API_SECRET', api_secret)
 
             # Use the CCXT library to create an exchange object
             exchange_class = getattr(ccxt, exchange_id.lower())
